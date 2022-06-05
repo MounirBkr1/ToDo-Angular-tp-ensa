79d8765aa5258a7919753d2a4a1b7c3afac32908
Mounir Bkr <miirbri1@gmail.com>
2022-06-05 14:30:59 +0100

    commit3 "final project"

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -11,7 +11,21 @@ import{Task} from 'src/app/models/task';
 })
 export class TasksComponent implements OnInit {
 
+  editForm=false;
+  showForm=false;
+
+  myTask:Task ={
+    label:'',
+    completed:false
+  }
   tasks:Task[]=[];
+  resultsTasks:Task[]=[];
+   searchText: string='';
+
+  editTask(task:any){
+    this.myTask=task;
+    this.editForm=true;
+  }
   constructor(private taskService:TaskService) { }
 
   ngOnInit(): void {
@@ -19,8 +33,59 @@ export class TasksComponent implements OnInit {
 
   }
 
+
+
+  deleteTask(id:any){
+    this.taskService.delete(id)
+      .subscribe(()=>{
+      this.tasks=this.tasks.filter(task=>task.id!=id);
+      this.getTasks();
+    });
+
+  }
+
+  persistTask(){
+    this.taskService.persist(this.myTask)
+      .subscribe((task:Task)=>{
+      this.tasks=[task,...this.tasks];
+      this.restTask();
+      this.showForm=false;
+        this.getTasks();
+    });
+
+  }
+
+  updateTask() {
+    this.taskService.update(this.myTask)
+      .subscribe(task =>{
+        this.restTask(),
+          this.editForm=false
+      })
+  }
+
+  restTask(){
+    this.myTask={
+      label:'',
+      completed:false
+    }
+  }
+
+  toggleCompleted(task:any){
+    this.taskService.completed(task.id,task.completed)
+      .subscribe(()=>{
+      task.completed=!task.completed
+    })
+  }
+
+
+  searchTasks(){
+    this.resultsTasks = this.tasks.filter((task)=>
+    task.label.toLocaleLowerCase().includes(this.searchText.toLocaleLowerCase()))
+  }
+
   getTasks(){
-    this.taskService.findAll().subscribe(tasks => this.tasks = tasks);
+    this.taskService.findAll().subscribe(tasks =>
+      this.resultsTasks = this.tasks = tasks);
   }
 
 }
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -13,4 +13,25 @@ export class TaskService {
   findAll(){
     return this.http.get<Task[]> ('http://localhost:3000/tasks');
   }
+
+  delete(id:any){
+    return this.http.delete(`http://localhost:3000/tasks/${id}`)
+  }
+
+  persist(task:any){
+    return this.http.post<Task>("http://localhost:3000/tasks",task);
+  }
+
+  completed(id:any,completed:any){
+    return this.http.patch(`http://localhost:3000/tasks/${id}`,{completed:!completed})
+  }
+
+  update(task:any){
+    return this.http.put(`http://localhost:5000/tasks/${task.id}`, task)
+  }
+
+
+
+
+
 }
